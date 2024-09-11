db000924c34ed929209acd0239dd2e06ff2507cd
Davy Ling <dsling@umd.edu>
2024-09-11 16:41:11 -0400

    ruby formatting

--- components/ui/entry.tsx
+++ components/ui/entry.tsx
@@ -1,11 +1,26 @@
 import { Ruby } from "./ruby";
 import { POS } from "../../app/types";
 
+function formatRuby(hanjaSplit: string[], hangulSplit: string[]): JSX.Element[] {
+    const rubyArray = [];
+    for (var i = 0; i < hanjaSplit.length; i++) {
+        if (hanjaSplit[i] === hangulSplit[i]) {
+            hangulSplit[i] = "";
+        }
+        rubyArray.push(<Ruby text={hanjaSplit[i]} ruby={hangulSplit[i]} />);
+    }
+    return rubyArray;
+}
+
 export function Entry({ hanja, hangul, definitions }: Readonly<{ hanja: string; hangul: string; definitions: Definition[] }>) {
+    const [hanjaSplit, hangulSplit] = [hanja.split(""), hangul.split("")];
+
     return (
         <div className="flex flex-row p-8 gap-x-20 bg-gray-800 rounded-lg w-full">
             <div className="flex items-center space-x-2">
-                <div className="text-4xl"><Ruby text={hanja} ruby={hangul} /></div>
+                <div className="text-4xl">
+                    {formatRuby(hanjaSplit, hangulSplit)}
+                    </div>
             </div>
             <div className="flex flex-col space-y-1">
                 <ol className="list-decimal list-inside">
