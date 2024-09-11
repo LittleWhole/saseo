9fa655e9a62d3ac7c74a31e501e35a2274c4f196
Davy Ling <dsling@umd.edu>
2024-09-11 12:21:38 -0400

    update update

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,7 +7,7 @@ import { POS } from "../types";
 export default function Page() {
   return (
     <div>
-      <div className="items-center p-10 bg-black text-white font-[family-name:var(--font-geist-sans)]">
+      <div className="items-center py-10 px-40 bg-black text-white font-[family-name:var(--font-geist-sans)]">
         <div className="flex items-center content-center justify-center space-x-6">
           <h1 className="font-bold text-2xl">
             <Ruby text="辭" ruby="사" />
@@ -19,7 +19,7 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <div className="flex items-center w-full p-10">
+      <div className="flex items-center w-full px-40 py-5">
 
         <Entry hanja="辭書" hangul="사서" definitions={[{pos: [POS.NOUN], text: "dictionary", examples: [""]}]} />
       </div>
--- components/ui/entry.tsx
+++ components/ui/entry.tsx
@@ -3,7 +3,7 @@ import { POS } from "../types";
 
 export function Entry({ hanja, hangul, definitions }: Readonly<{ hanja: string; hangul: string; definitions: [{ pos: POS[], text: string, examples: string[] }] }>) {
     return (
-        <div className="flex flex-row p-8 gap-x-40 bg-gray-800 rounded-lg w-full">
+        <div className="flex flex-row p-8 gap-x-20 bg-gray-800 rounded-lg w-full">
             <div className="flex items-center space-x-2">
                 <div className="text-4xl"><Ruby text={hanja} ruby={hangul} /></div>
             </div>
